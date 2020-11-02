d8bbe59a31490cd8c8deac27cf4d1caf2b3e35ab
viest <dev@service.viest.me>
2020-11-02 22:01:37 +0800

    boost

--- Formula/boost@1.73.rb
+++ Formula/boost@1.73.rb
@@ -1,4 +1,4 @@
-class Boost < Formula
+class BoostAT173 < Formula
   desc "Collection of portable C++ source libraries"
   homepage "https://www.boost.org/"
   url "https://downloads.sourceforge.net/project/boost/boost/1.73.0/boost_1_73_0.tar.bz2"
---
+++ README.md
@@ -0,0 +1,13 @@
+# Homebrew
+
+## Add Repositories
+
+```bash
+brew tap viest/homebrew https://github.com/viest/homebrew.git
+```
+
+## Install
+
+```bash
+brew install viest/homebrew/boost@1.73
+```
